a642ed4b0f83fc04a137d655da212676a1905e4c
Hiroshi MITANI <hmitani0429@gmail.com>
2017-11-21 14:37:17 +0900

    Chart.jsとxml-loaderを削除

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -1,33 +1,14 @@
 <template lang="pug">
-canvas.line-chart
+h1 {{ msg }}
 </template>
 
 <script>
-import Chart from 'chart.js'
 export default {
   name: 'LineChart',
   data () {
     return {
       msg: 'Line Graph'
     }
-  },
-  mounted() {
-    let vm = this
-    let ctx = vm.$el.getContext('2d')
-    let myChart = new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-          label: 'My First dataset',
-          backgroundColor: 'rgba(255, 99, 132, 0.5)',
-          borderColor: 'rgb(255, 99, 132)',
-          lineTension: 0.2,
-          data: [0, 10, 5, 2, 20, 30, 45]
-        }]
-      }
-    })
-    console.log(myChart)
   }
 }
 </script>
@@ -36,9 +17,5 @@ export default {
 <style scoped lang="scss">
 h1, h2 {
   font-weight: normal;
-}
-
-canvas {
-  background: #eee;
 }
 </style>
